f8c08b0708380bdc88161fbeeaee65ed66d7fe64
Drew Overcash <drewovercash@gmail.com>
2017-10-29 17:48:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,13 +31,20 @@ function currentLine(line) {
 //Provided Example Code//
 var katzDeliLine = [];
 
-takeANumber(katzDeliLine, "Ada");
-takeANumber(katzDeliLine, "Grace");
-takeANumber(katzDeliLine, "Kent");
-currentLine(katzDeliLine);
-nowServing(katzDeliLine);
-currentLine(katzDeliLine);
-takeANumber(katzDeliLine, "Matz");
-currentLine(katzDeliLine);
-nowServing(katzDeliLine);
-currentLine(katzDeliLine);
+takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
+takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
+
+currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+
+nowServing(katzDeliLine); // "Currently serving Ada."
+
+currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
+
+takeANumber(katzDeliLine, "Matz"); // "3"
+
+currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
+
+nowServing(katzDeliLine); // "Currently serving Grace."
+
+currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
